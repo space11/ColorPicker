d048ca3636432d9f127525cd52561b1186f18d49
Borys Olejniczak <borys.olejniczak@gmail.com>
2019-05-02 14:25:05 +0200

    Add callback to dropdown menu

--- src/ColorPicker/index.js
+++ src/ColorPicker/index.js
@@ -1,16 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Dropdown, Button, Icon } from "antd";
+import { Dropdown, Button, Icon, Menu } from "antd";
 import "antd/dist/antd.css";
-import ColorMenu from "./ColorMenu";
+import ColorBlock from "./ColorBlock";
+import ColorsList from "./editorColors";
 
-const ColorPicker = ({ icon }) => (
-  <Dropdown overlay={ColorMenu}>
-    <Button>
-      <Icon type={icon} /> <Icon type="down" />
-    </Button>
-  </Dropdown>
-);
+const ColorItem = ({ colorHex }) => <ColorBlock colorHex={colorHex} />;
+
+const ColorPicker = ({ icon, onColorSelect }) => {
+  const onColorSelectLocal = event => {
+    const selectedColor = event.key;
+    onColorSelect(selectedColor);
+  };
+  
+  const menu = (
+    <Menu onClick={onColorSelectLocal}>
+      {ColorsList.map(color => (
+        <Menu.Item
+          key={color.hexValue}
+          label={color.label}
+          data-colorvalue={color.hexValue}
+        >
+          <ColorItem colorHex={color.hexValue} />
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={menu}>
+      <Button>
+        <Icon type={icon} /> <Icon type="down" />
+      </Button>
+    </Dropdown>
+  );
+};
 
 ColorPicker.propTypes = {
   icon: PropTypes.string
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import ColorPicker from "./ColorPicker";
 import * as colorPickerIcons from "./ColorPicker/iconsColorPicker";
 
+const exampleCallback = e => console.log(`Example Callback. e: ${e}`);
+
 ReactDOM.render(
-  <ColorPicker icon={colorPickerIcons.ICON_FONT_COLORS} />,
+  <ColorPicker
+    icon={colorPickerIcons.ICON_FONT_COLORS}
+    onColorSelect={exampleCallback}
+  />,
   document.getElementById("container")
 );
